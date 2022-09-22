dc2c53c66ed02b88379b0d494fc610537c81aed5
example <example@example.com>
2022-09-22 11:49:02 +0800

    面经管理布局

--- src/views/article.vue
+++ src/views/article.vue
@@ -1,13 +1,110 @@
 <template>
-  <div>面经管理</div>
+  <div class="article-page">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>面经后台</el-breadcrumb-item>
+      <el-breadcrumb-item>面经管理</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card shadow="never" border="false">
+      <template #header>
+        <div class="header">
+          <span>共 300 条记录</span>
+          <el-button icon="el-icon-plus" size="small" type="primary" round>
+            添加面经
+          </el-button>
+        </div>
+      </template>
+
+      <el-table :data="tableData" border style="width: 100%">
+        <el-table-column prop="date" label="日期" width="180">
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="180">
+        </el-table-column>
+        <el-table-column prop="address" label="地址"> </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "article-page",
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
+  },
+  created() {},
+  methods: {},
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.el-card {
+  margin-top: 25px;
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-right: 16px;
+  }
+  .actions {
+    font-size: 18px;
+    display: flex;
+    justify-content: space-around;
+    color: #666;
+    > i:hover {
+      color: rgba(114, 124, 245, 1);
+      cursor: pointer;
+    }
+  }
+}
+.el-pagination {
+  margin-top: 20px;
+  text-align: center;
+}
+.el-breadcrumb {
+  margin-top: 10px;
+}
+.el-form {
+  padding-right: 40px;
+}
+.quill-editor {
+  ::v-deep .ql-editor {
+    height: 300px;
+  }
+}
+.el-rate {
+  padding: 10px 0;
+}
+.article-preview {
+  padding: 0 40px 40px 40px;
+  > h5 {
+    font-size: 20px;
+    color: #666;
+    border-bottom: 1px dashed #ccc;
+    padding-bottom: 30px;
+    margin: 0 0 20px 0;
+  }
+}
 </style>
