c9b884bd4b4c5f3bba0710f7ea2c8672decd9d0d
example <example@example.com>
2022-09-22 16:10:41 +0800

    添加面经-增加表单校验

--- src/views/article.vue
+++ src/views/article.vue
@@ -39,15 +39,15 @@
     </el-card>
 
     <el-drawer title="添加面经" :visible.sync="showDrawer" size="50%">
-      <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="标题">
+      <el-form ref="form" :rules="rules" :model="form" label-width="80px">
+        <el-form-item label="标题" prop="stem">
           <el-input v-model="form.stem"></el-input>
         </el-form-item>
-        <el-form-item label="内容">
+        <el-form-item label="内容" prop="content">
           <el-input v-model="form.content"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary">确认</el-button>
+          <el-button type="primary" @click="submit">确认</el-button>
           <el-button>取消</el-button>
         </el-form-item>
       </el-form>
@@ -69,6 +69,10 @@ export default {
       form: {
         stem: "",
         content: "",
+      },
+      rules: {
+        stem: [{ required: true, message: "请输入标题", trigger: "blur" }],
+        content: [{ required: true, message: "请输入内容", trigger: "blur" }],
       },
     };
   },
@@ -93,6 +97,16 @@ export default {
     },
     addArticle() {
       this.showDrawer = true;
+    },
+    submit() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     },
   },
 };
